48af721f0caf716e9ccf4a1d672e43aa52a20f2f
Abraham-7 <noria0540@hotmail.com>
2016-01-04 09:52:51 -0500

    Initial commit

---
+++ android/src/com/mygdx/tube2/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.tube2.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.tube2.ForceTube2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ForceTube2(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/tube2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tube2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tube2.ForceTube2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ForceTube2(), config);
+	}
+}
